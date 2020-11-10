ec9ed12e766255adacff1efa63eced08b1d6c1bd
Tomás Generelo <tgenerelo@gmail.com>
2020-11-11 00:15:32 +0100

    Añadido EjemploVectores

---
+++ Ejercicios1/src/Ejercicios1_3/EjemploVectores.java
@@ -0,0 +1,24 @@
+package Ejercicios1_3;
+
+public class EjemploVectores {
+
+	public static void main(String[] args) {
+		
+		int fil=4;
+		int col=5;
+		int vNumeros[][] = new int[fil][col];
+		
+
+		
+		for (int i=0; i<tam; i++) {
+			System.out.println(vNumeros[i]);
+		}
+		
+		
+		for (int i=0; i<vNumeros.length; i++) {
+			for (int j=0; i<vNumeros[i].length; j++)
+		}
+
+	}
+
+}
